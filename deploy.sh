24fe5163011cf20120cf854e2f6be4bacc230f55
Sogrey <suzhaoheng2006@163.com>
2022-02-17 10:39:53 +0800

    deploy master

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,6 @@ echo -e '\033[32;40m
 echo -e "\033[32;40m [1/4] \033[0m init..."
 
 # npm install
-# npm install -g vuepress
 # npm audit fix
 
 echo -e "\033[32;40m [2/4] \033[0m commit 2 master branch"
